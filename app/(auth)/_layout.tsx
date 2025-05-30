27d85e833416e76808af52d7e75bc5d425181787
Babita00 <babitaasth07@gmail.com>
2025-05-30 13:06:04 +0545

    style: clean up formatting in AuthLayout component

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,10 +3,9 @@ import { Stack } from "expo-router";
 export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index"  />
-      <Stack.Screen name="register"  />
-      {/* other auth screens */}
+      <Stack.Screen name="index"/>
+      <Stack.Screen name="register" />
+  
     </Stack>
-
   );
 }
