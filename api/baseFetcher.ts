85197b3a2731cdd0ad7dc931c6f950da5c9831b0
Babita00 <babitaasth07@gmail.com>
2025-06-20 22:30:07 +0545

    fix: Update baseFetcher to include token handling in request headers

--- api/baseFetcher.ts
+++ api/baseFetcher.ts
@@ -1,17 +1,25 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-export const BASE_URL = "https://athleticore-backend-1.onrender.com/api";
-// export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL = "https://athleticore-backend-1.onrender.com/api";
+export const BASE_URL = "http://localhost:5000/api";
+
+export const getAccessToken = () => {
+  return localStorage.getItem("accessToken");
+};
 
 export const baseFetcher = async <T>(
   endpoint: string,
   options: AxiosRequestConfig = {}
 ): Promise<T> => {
+  const token = await getAccessToken();
+  console.log("🛂 Sending token:", token);
+
   const res = await axios({
     url: `${BASE_URL}${endpoint}`,
     method: options.method || "GET",
     headers: {
       "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
       ...options.headers,
     },
     ...options,
