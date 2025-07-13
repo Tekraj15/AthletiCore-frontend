960e226747e0693bb018c8c2e736e05a0ed467fa
Tek Raj Awasthi <tekrajawasthi@Teks-MacBook-Air-3.local>
2025-07-13 23:01:24 +0200

    Added announcement update and delete URLs

--- constants/url/url.ts
+++ constants/url/url.ts
@@ -5,7 +5,10 @@ export const GET_MY_EVENT_URL = "/event/my-events";
 export const GET_EVENT_URL = "/event/";
 // export const GET_EVENT_BY_ID_URL = "/event/{id}";
 export const CREATE_ANNOUNCEMENT_URL = "/announcement/";
-export const GET_ANNOUNCEMENT_URL = "/announcement/";
+export const GET_ANNOUNCEMENT_URL = "/announcement";
+// Add announcement update and delete URLs
+export const UPDATE_ANNOUNCEMENT_URL = "/announcement";
+export const DELETE_ANNOUNCEMENT_URL = "/announcement";
 // custom form,
 export const CREATE_EVENT_FORM_URL = "/form-events/:eventId/forms";
 //register for event
